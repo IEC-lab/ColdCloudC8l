26e23f19c35de2d70ca059af8a317a137b1a8bb2
unknown <770340084@qq.com>
2021-04-23 17:11:17 +0800

    modify common config

--- src/components/Common.vue
+++ src/components/Common.vue
@@ -2,10 +2,10 @@
     const APIURL = "/api"
     const STREAMSURL = APIURL + "/streams"
     const GRAPHQLURL = APIURL + "/graphql/query"
-    const COLDBRIDGEURL = "ws://coldbridge-service.coldworld:8888/rtsp/1/"
-    // const COLDBRIDGEURL = "ws://192.168.1.4:8888/rtsp/1/"
-    const STRUCTEDMSGSWSURL = "ws://coldmountain-service.coldworld:8899/api/getStructedMsgs"
-    // const STRUCTEDMSGSWSURL = "ws://192.168.1.4:8899/api/getStructedMsgs"
+    // const COLDBRIDGEURL = "ws://coldbridge-service.coldworld:8888/rtsp/1/"
+    const COLDBRIDGEURL = "ws://192.168.1.4:8888/rtsp/1/"
+    // const STRUCTEDMSGSWSURL = "ws://coldmountain-service.coldworld:8899/api/getStructedMsgs"
+    const STRUCTEDMSGSWSURL = "ws://192.168.1.4:8899/api/getStructedMsgs"
     export default {
         APIURL,
         STREAMSURL,
